63d671bd7c0eaab6c91c0067f8d7d4cca209ca1e
saurav-sigmoid1 <sauravv1@sigmoidanalytics.com>
2022-02-26 13:49:22 +0530

    change class name and add mock test

--- codewithtest/code_part.py
+++ codewithtest/code_part.py
@@ -1,7 +1,11 @@
 import sys
-class oddTimeNumberOccurance:
-    def getOddOccurrence(self,arr, arr_size):
+
+
+class  OddOccuranceCheck:
+
+    def Check(self, arr, arr_size):
         """iterate over the array to check the number count occur even or odd times if there is no number occure odd time return -1"""
+
         for idx1 in range(0, arr_size):
             count = 0
             for idx2 in range(0, arr_size):
@@ -12,20 +16,25 @@ class oddTimeNumberOccurance:
                 return arr[idx1]
         return -1
 
+    def readFromFile(self,filename):
+        infile = open(filename,'r')
+        line = infile.readlines()
+        return line
 
 
-# # driver code
+# driver code
+
 if __name__ == "__main__":
-    arr=[]
-    inputs=[]
+    arr = []
+    inputs = []
     sys.stdin = open('test.txt', 'r')
     """reading the input from the file and call the function to perform on the array"""
     for i in range(5):
         inputs = input()
         for val in inputs:
-            if val!=' ' and val!='\n':
+            if val != ' ' and val != '\n':
                 arr.append(int(val))
         n = len(arr)
         obj = oddTimeNumberOccurance()
         print(obj.getOddOccurrence(arr, n))
-        arr=[]
+        arr = []
--- codewithtest/test_file.py
+++ codewithtest/test_file.py
@@ -1,13 +1,14 @@
 import pytest
-from codewithtest.code_part import oddTimeNumberOccurance
+from unittest.mock import MagicMock
+from codewithtest.code_part import OddOccuranceCheck
 
 
-class TestClass:
+class Test_OddOccuranceCheck:
 
     @pytest.fixture()
     def numberOccurance(self):
-        numberoccurance = oddTimeNumberOccurance()
-        return numberoccurance
+        numberoccurances = OddOccuranceCheck()
+        return numberoccurances
 
     def setup_class(self):
         self.testfile = open('test.txt','r')
@@ -16,17 +17,27 @@ class TestClass:
     def teardown_class(self):
         self.testfile.close()
 
+    def test_mock_readFromFile(self,numberOccurance,monkeypatch):
+        mock_file = MagicMock()
+        mock_file.readlines = MagicMock(return_value="test data")
+        mock_open = MagicMock(return_value=mock_file)
+        monkeypatch.setattr("builtins.open",mock_open)
+        result = numberOccurance.readFromFile("tes")
+        mock_open.assert_called_once_with("tes",'r')
+        assert result == "test data"
+
+
     def test_callfunction(self,numberOccurance):
         assert numberOccurance.getOddOccurrence([1,2,3,4],4)
 
     def test_number_occur_odd_number_of_time(self,numberOccurance):
         arr =[1,1,2,2,3,3,3]
-        val = numberOccurance.getOddOccurrence(arr,7)
+        val = numberOccurance.check(arr,7)
         assert  val == 3
 
     def test_only_single_elementPresent(self,numberOccurance):
         arr=[1]
-        val = numberOccurance.getOddOccurrence(arr,1)
+        val = numberOccurance.check(arr,1)
         assert val == 1
 
     def test_no_number_occur_odd_times(self,numberOccurance):
@@ -41,7 +52,7 @@ class TestClass:
 
     def test_array_is_empty(self,numberOccurance):
         arr = []
-        val= numberOccurance.getOddOccurrence(arr,0)
+        val = numberOccurance.getOddOccurrence(arr,0)
         assert val == -1
 
 
